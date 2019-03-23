9ab29abfe37bfbe151a057a0ebc41db0f53d2982
Jason Burns <jasonb315@protonmail.com>
2019-03-23 15:21:53 -0700

    ini w/starter

---
+++ Lab04_TicTacToe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab04_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PlayGame();
+        }
+
+        static void PlayGame()
+        {
+            // TODO: Setup your game here. Create an introduction. 
+            // Create your players, and instantiate your Game class. 
+            // output to the console the winner
+        }
+    }
+}
